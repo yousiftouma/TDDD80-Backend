528d3196d596d6eacb0c58dd8f1c6574383b9e82
Yousif Touma <youto814@student.liu.se>
2015-04-20 14:32:10 +0200

    testing

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import os
-from setuptools.compat import execfile
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR', '.'), 'virtenv')
+
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
 
-from app.__init__ import app as application
+from app import app as application
 
-application.run(4599, debug=True)
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 4599, application)
+    # Wait for a single request, serve it and quit.
+    httpd.serve_forever()
+
