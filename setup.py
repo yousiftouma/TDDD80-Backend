ee998021de6dcb75c038ab8522db8100d3945468
Yousif Touma <youto814@student.liu.se>
2015-04-20 15:39:00 +0200

    hej

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='MyApp',
+setup(name='MyApplication',
       version='1.0',
       description='OpenShift App',
       author='Yousif Touma',
       author_email='youto814@student.liu.se',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask'],
+      install_requires=['Flask=0.10.0', 'Flask-SQLAlchemy=2.0'],
       )
