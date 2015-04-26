6488e50fa7fffa703b4a91bb4ba3f390abd2a9ad
unknown <youto814@student.liu.se>
2015-04-26 21:20:37 +0200

    printing

--- app/rest_api.py
+++ app/rest_api.py
@@ -82,6 +82,7 @@ def get_number_of_likes(post_id):
 @app.route('/get_comments_for_post_by_id/<post_id>', methods=['GET'])
 def get_comments_for_post(post_id):
     comments = get_comments_for_post_by_id(post_id)
+    print("apifunction gets comments: ", comments)
     result = []
     if comments is not None:
         for comment in comments:
