e8d5e92de29f426aab9e1cbe2bf5690897fd34bf
Yousif Touma <youto814@student.liu.se>
2015-04-28 09:10:21 +0200

    no

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ __author__ = 'Yousif Touma'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = os.environ['OPENSHIFT_SQLITE_DB_URL']
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-#'sqlite:///' + os.path.join(basedir, 'database.db')
